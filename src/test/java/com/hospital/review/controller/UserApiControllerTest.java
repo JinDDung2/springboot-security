372a90e4a66440b91a95d00335706143554fb140
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-28 15:43:19 +0900

    [Feat] 회원 가입 성공, 실패 테스트

---
+++ src/test/java/com/hospital/review/controller/UserApiControllerTest.java
@@ -0,0 +1,71 @@
+package com.hospital.review.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hospital.review.domain.dto.UserDto;
+import com.hospital.review.domain.dto.UserJoinRequest;
+import com.hospital.review.exception.ErrorCode;
+import com.hospital.review.exception.HospitalReviewException;
+import com.hospital.review.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserApiController.class)
+class UserApiControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    UserService userService;
+
+    @Test
+    void 회원가입_성공() throws Exception {
+
+        UserJoinRequest userJoinRequest = new UserJoinRequest();
+        userJoinRequest.setUsername("KimMinJae");
+        userJoinRequest.setPassword("nice");
+        userJoinRequest.setEmail("winner@korea.ac.kr");
+
+        when(userService.join(any())).thenReturn(mock((UserDto.class)));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+
+    }
+
+    @Test
+    void 회원가입_실패() throws Exception {
+        UserJoinRequest userJoinRequest = new UserJoinRequest();
+        userJoinRequest.setUsername("KimMinJae");
+        userJoinRequest.setPassword("nice");
+        userJoinRequest.setEmail("winner@korea.ac.kr");
+
+        when(userService.join(any())).thenThrow(new HospitalReviewException(ErrorCode.DUPLICATED_USER_NAME, ""));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isConflict());
+
+    }
+}
