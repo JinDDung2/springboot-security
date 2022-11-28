cbdd5982c67fb667613d74a2fdaeadc4ef661942
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-28 14:10:44 +0900

    [Add] UserRepository 추가

---
+++ src/main/java/com/hospital/review/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hospital.review.repository;
+
+import com.hospital.review.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
