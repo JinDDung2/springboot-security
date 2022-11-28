3d5f7562ec93faeee7f07bc474068df15224e069
jinhyuck <gjwlsgur4866@gmail.com>
2022-11-28 15:02:15 +0900

    [Feat] 회원가입 중복체크 예외처리(예외코드, 예외메세지, 예외클래스)

--- src/main/java/com/hospital/review/service/UserService.java
+++ src/main/java/com/hospital/review/service/UserService.java
@@ -3,6 +3,8 @@ package com.hospital.review.service;
 import com.hospital.review.domain.User;
 import com.hospital.review.domain.dto.UserDto;
 import com.hospital.review.domain.dto.UserJoinRequest;
+import com.hospital.review.exception.ErrorCode;
+import com.hospital.review.exception.HospitalReviewException;
 import com.hospital.review.repository.UserRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +18,12 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    /** 회원가입 */
     public UserDto join(UserJoinRequest request) {
         // 유저네임 중복체크
         // 중복 되어있으면 Exception 처리 발생
         userRepository.findByUsername(request.getUsername())
                 .ifPresent(user -> {
-                    throw new RuntimeException("username이 중복됩니다.");
+                    throw new HospitalReviewException(ErrorCode.DUPLICATED_USER_NAME, String.format("username:%s", request.getUsername()));
                 });
 
 
