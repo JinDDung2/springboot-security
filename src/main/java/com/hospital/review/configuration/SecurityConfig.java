aaeeb0dddfe669feeb8605de9f8ad37e264e8f14
jinhyuck <gjwlsgur4866@gmail.com>
2022-12-05 11:29:03 +0900

    [Feat] Security 설정 수정 1. 회원가입과 로그인은 모두 허용 2. 댓글등록은 인가받은 사용자만 허용

--- src/main/java/com/hospital/review/configuration/SecurityConfig.java
+++ src/main/java/com/hospital/review/configuration/SecurityConfig.java
@@ -2,11 +2,11 @@ package com.hospital.review.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,8 +19,9 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeHttpRequests()
-                .antMatchers("/api/**").permitAll()
                 .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                // permitAll 다음에 와야함. 순서대로 허가가 떨어지기 때문
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
